fa24d9fcda8804ea421f767a33885cfebfa18961
wwxfromtju <wxwang@tju.edu.cn>
2016-09-01 21:33:48 +0800

    random go

--- simple-vis/mpl_squares.py
+++ simple-vis/mpl_squares.py
@@ -3,7 +3,16 @@
 
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(squares)
+
+plt.plot(input_values, squares, linewidth=5)
+
+plt.title('just title', fontsize=24)
+plt.xlabel('x axis', fontsize=24)
+plt.ylabel('y axis', fontsize=24)
+
+plt.tick_params(axis='both', labelsize=24)
+
 plt.show()

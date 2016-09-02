134ab5b4f39176274593e98b76ea54a40420fcc0
wwxfromtju <wxwang@tju.edu.cn>
2016-09-02 23:25:04 +0800

    aaaa

---
+++ simple-vis/hn_submissions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import requests
+
+from operator import itemgetter
+
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+
+submission_ids = r.json()
+print(submission_ids)
+submission_dicts = []
+
+for submission_id in submission_ids[:30]:
+    url = ('https://hacker-news.firebaseio.com/v0/item/' + str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.json())
+    response_dict = submission_r.json()
+
+    submission_dict = {
+        'title': response_dict['title'],
+        'link': 'http://news.ycombinator.com/item?id=' + str(submission_id),
+        'comments': response_dict.get('descendants', 0)
+    }
+
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print(submission_dict['title'])
+    print(submission_dict['link'])
+    print(submission_dict['comments'])
